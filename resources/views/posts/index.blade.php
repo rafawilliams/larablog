644032de50986a10faf0112be6f9cd4d1d57a97d
Rafael Williams <rafael.williams@gmail.com>
2017-02-11 12:37:34 -0500

    add index view

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,19 @@
+@extends('layout.master')
+
+
+@section('content')
+<div class="col-md-8 blog-main">
+
+        @foreach($posts as $post)
+            @include('posts.posts', $post)
+        @endforeach
+
+          <nav class="blog-pagination">
+            <ul class="pager">
+              <li><a class="btn btn-outline-primary" href="#">Previous</a></li>
+              <li><a class="btn btn-outline-secondary disabled" href="#">Next</a></li>
+            </ul>
+          </nav>
+
+</div><!-- /.blog-main -->
+@endsection
